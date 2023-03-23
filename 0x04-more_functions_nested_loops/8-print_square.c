11386c8f0b034bb783b21b9bbe66dbf082d5ac25
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2023-03-23 12:57:17 +0000

    Patched Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,12 +15,17 @@ void print_square(int size)
     if (size <= 0)
     {
         _putchar('\n');
+        return;
     }
     else
     {
         for (row = 1; row <= size; row++)
+        {
             for (column = 1; column <= size; column++)
+            {
                 _putchar('#');
+            }
             _putchar('\n');
+        }
     }
 }
