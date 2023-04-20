ae59e0db1a9c513e4f914727b44c14ab999d1268
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2023-04-20 20:36:17 +0000

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,23 +3,24 @@
 /**
  * sum_them_all - a function that returns the sum of all its parameters.
  * @n: unsigned int.
- * Return: sum of parameters or 0 if n == 0.
+ *
+ * Return: sum of parameters or 0.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
+	va_list ap;
 	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(args, n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
+		sum += va_arg(ap, int);
 
-	va_end(args);
+	va_end(ap);
 
 	return (sum);
 }
