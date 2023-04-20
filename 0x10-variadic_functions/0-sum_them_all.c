dac5c9e6beee3ff996af90e092de9268f7ca1c9d
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2023-04-20 18:04:29 +0000

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,17 +7,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum;
-	unsigned int i;
 	va_list ap;
+	unsigned int i;
+	int sum;
 
 	va_start(ap, n);
 
 	if (n == 0)
+	{
 		return (0);
+	}
 
 	for (; i < n; i++)
+	{
 		sum += va_arg(ap, int);
+	}
 
 	va_end(ap);
 
