e0d8a059ef8c9fe2bdbdd24824efae3502a15ae8
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2023-05-04 15:27:03 +0100

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(8UL) * 8)
 	{
-		return -1;
+		return (-1);
 	}
 
-	return (n >> index) & 1;
+	return ((n >> index) & 1);
 }
